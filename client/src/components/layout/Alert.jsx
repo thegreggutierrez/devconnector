3ab99eefbd5cc69d016e85e72f78ea9e40a3880e
Gregory Gutierrez <Gregory.Gutierrez@nafinc.com>
2022-11-05 08:43:54 -0700

    Added React User Authentication.

--- client/src/components/layout/Alert.jsx
+++ client/src/components/layout/Alert.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Alert = ({ alerts }) =>
-alerts !== null && alerts.length > 0 && alerts.map(alert => (
-  <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-    {alert.msg}
+const Alert = ({ alerts }) => (
+  <div className="alert-wrapper">
+    {alerts.map((alert) => (
+      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        {alert.msg}
+      </div>
+    ))}
   </div>
-));
+);
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   alerts: state.alert
 });
 
