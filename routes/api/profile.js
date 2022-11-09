0b165820538d4d88d873c8729c8841c4394c75bc
Gregory Gutierrez <Gregory.Gutierrez@nafinc.com>
2022-11-09 13:30:06 -0800

    Added Profile Display, Fixed Profile State Issue, and Updated GitHub API Authentication.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -334,11 +334,16 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @access   Public
 router.get('/github/:username', (req, res) => {
     try {
-        const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
-            method: 'GET',
-            headers: { 'user-agent': 'node.js' }
-        };
+      const options = {
+        uri: encodeURI(
+          `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+        ),
+        method: 'GET',
+        headers: {
+          'user-agent': 'node.js',
+          Authorization: `token ${config.get('githubToken')}`
+        }
+      };
 
         request(options, (error, response, body) => {
             if (error) console.error(error);
